620ed0e487c734fa7e769228c2ddacaefca4d9ca
devGusta2 <gustavo.souza2979@gmail.com>
2024-05-27 18:29:29 -0300

    Aula02 - Organizar Projeto

--- aula02/src/App.js
+++ aula02/src/App.js
@@ -7,9 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Hello World
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Introdução ao React.js</p>
         <a
           className="App-link"
           href="https://reactjs.org"
