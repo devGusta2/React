5bf866b87b48fb36d670b5e856b240e21f1f15fc
devGusta2 <gustavo.souza2979@gmail.com>
2024-05-28 22:09:44 -0300

    Aula03 - Componentes

--- aula03/src/Components/Header/index.js
+++ aula03/src/Components/Header/index.js
@@ -1,5 +1,14 @@
 function Header(){
-    
+    return(
+        //JSX HTML COM CSS DENTRO DO JS isso é o JSX
+        <header>
+            <span>GUSTA</span>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Assistir</a>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
