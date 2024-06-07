e15a2069c8ffba78d1ee0c088095aa86e554b9c7
devGusta2 <gustavo.souza2979@gmail.com>
2024-06-06 23:15:32 -0300

    Aula03 - Componentes

--- aula03/src/Components/Header/index.js
+++ aula03/src/Components/Header/index.js
@@ -1,6 +1,10 @@
 function Header(){
     return(
         //JSX HTML COM CSS DENTRO DO JS isso é o JSX
+
+
+        // no REACT utilizamos class name o que no html seria só class
+        // pois no react class é uma palavra reservada
         <header className="">
             <span>GUSTA</span>
             <nav>
