042fc4d434fcb3ba3db5f430007bc52cc9a429e9
devGusta2 <gustavo.souza2979@gmail.com>
2024-07-01 19:11:07 -0300

    Aula04 - Componentes props

---
+++ aula04/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css';
+
+function Footer(){
+
+    return(
+        <footer className={styles.footer}>
+            <h2>Gusta dev 2 &copy; Desenvolvido por gustavo rodrigues</h2>
+        </footer>
+    );
+}
+
+export default Footer;
