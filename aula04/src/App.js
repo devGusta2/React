0d659afa6a01f2fec4d280da7a981995fdc4522f
devGusta2 <gustavo.souza2979@gmail.com>
2024-07-02 18:39:41 -0300

    Aula04 - Componentes props

--- aula04/src/App.js
+++ aula04/src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Banner from './components/Banner';
 
 
 function App() {
   return (
     <div className="App">
         <Header/>
+       
         <h1>Olá</h1>
+        <Banner/>
         <Footer/>
     </div>
   );
---
+++ aula04/src/components/Banner/index.js
@@ -0,0 +1,9 @@
+import styles from './Banner.module.css';
+
+function banner(){
+    return(
+        <h1>Banner funcionando</h1>
+    );
+}
+
+export default banner;
